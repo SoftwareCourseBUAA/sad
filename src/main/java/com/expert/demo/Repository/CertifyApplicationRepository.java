0052bec2e611744df5d8f3f10d29f643ace255e4
zzy <ilmare@buaa.edu.cn>
2018-06-19 13:02:46 +0800

    admin-application-server

--- src/main/java/com/expert/demo/Repository/CertifyApplicationRepository.java
+++ src/main/java/com/expert/demo/Repository/CertifyApplicationRepository.java
@@ -4,7 +4,13 @@ import com.expert.demo.Entity.CertifyApplication;
 import com.expert.demo.Entity.MatchApplication;
 import com.expert.demo.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CertifyApplicationRepository extends JpaRepository<CertifyApplication,Integer> {
    public CertifyApplication findByUser(User user);
+
+   public CertifyApplication findByApplyId(int appid);
+
+   @Query(value="select count(a) from CertifyApplication a where a.applyId>0")
+   public int getAppNumber();
 }
